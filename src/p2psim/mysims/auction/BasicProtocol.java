46d01e3e3d4f1cd89805c2c75dcaf42377a495c4
Stefan Zasada <sjzasada@gmail.com>
2010-08-05 14:46:17 +0100

    modified package structure

---
+++ src/p2psim/mysims/auction/BasicProtocol.java
@@ -0,0 +1,14 @@
+/*
+ * Created on 16:36:37 14 May 2009
+ * Project: PeerSims 
+ * File: BasicProtocol.java
+ * 
+ * @author stefan
+ * 
+ * TODO: 
+ */
+package p2psim.mysims.auction;
+
+public class BasicProtocol {
+
+}
---
+++ src/p2psim/mysims/hub/BasicProtocol.java
@@ -0,0 +1,14 @@
+/*
+ * Created on 16:37:54 14 May 2009
+ * Project: PeerSims 
+ * File: BasicProtocol.java
+ * 
+ * @author stefan
+ * 
+ * TODO: 
+ */
+package p2psim.mysims.hub;
+
+public class BasicProtocol {
+
+}
