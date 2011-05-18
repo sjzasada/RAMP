edb73298acb8ee0232b58bfdaaf8826e0ba905fa
Stefan Zasada <sjzasada@gmail.com>
2011-05-18 14:48:34 +0100

    added gui

---
+++ src/uk/ac/ucl/chem/ccs/ramp/user/UserGui.java
@@ -0,0 +1,49 @@
+package uk.ac.ucl.chem.ccs.ramp.user;
+
+import org.eclipse.swt.widgets.Dialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class UserGui extends Dialog {
+
+	protected Object result;
+	protected Shell shell;
+
+	/**
+	 * Create the dialog.
+	 * @param parent
+	 * @param style
+	 */
+	public UserGui(Shell parent, int style) {
+		super(parent, style);
+		setText("SWT Dialog");
+	}
+
+	/**
+	 * Open the dialog.
+	 * @return the result
+	 */
+	public Object open() {
+		createContents();
+		shell.open();
+		shell.layout();
+		Display display = getParent().getDisplay();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Create contents of the dialog.
+	 */
+	private void createContents() {
+		shell = new Shell(getParent(), getStyle());
+		shell.setSize(450, 300);
+		shell.setText(getText());
+
+	}
+
+}
