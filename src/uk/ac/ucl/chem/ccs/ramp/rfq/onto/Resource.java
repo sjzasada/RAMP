125b96eed5ca8b4384be0c8f50a999f84423050b
Stefan Zasada <sjzasada@gmail.com>
2011-06-03 16:49:33 +0100

    started onto

---
+++ src/uk/ac/ucl/chem/ccs/ramp/rfq/onto/Resource.java
@@ -0,0 +1,30 @@
+package uk.ac.ucl.chem.ccs.ramp.rfq.onto;
+
+public class Resource {
+
+	String RESOURCE;
+	String REQUESTID;
+	String CPUCOUNT;
+	public String getRESOURCE() {
+		return RESOURCE;
+	}
+	public void setRESOURCE(String rESOURCE) {
+		RESOURCE = rESOURCE;
+	}
+	public String getREQUESTID() {
+		return REQUESTID;
+	}
+	public void setREQUESTID(String rEQUESTID) {
+		REQUESTID = rEQUESTID;
+	}
+	public String getCPUCOUNT() {
+		return CPUCOUNT;
+	}
+	public void setCPUCOUNT(String cPUCOUNT) {
+		CPUCOUNT = cPUCOUNT;
+	} 
+	
+	
+	
+	
+}
