5ba957ed1239dc51bc3ddf927c0a646c2235f437
Stefan Zasada <sjzasada@gmail.com>
2011-06-10 10:27:30 +0100

    fixed missing onto file

---
+++ src/uk/ac/ucl/chem/ccs/ramp/rfq/onto/Resource.java
@@ -0,0 +1,60 @@
+package uk.ac.ucl.chem.ccs.ramp.rfq.onto;
+
+
+
+/**
+* Protege name: Resource
+* @author OntologyBeanGenerator v4.1
+* @version 2011/06/10, 10:25:58
+*/
+public interface Resource extends jade.content.Concept {
+
+   /**
+   * Protege name: TOTALDISKSPACE
+   */
+   public void setTOTALDISKSPACE(String value);
+   public String getTOTALDISKSPACE();
+
+   /**
+   * Protege name: RAMPERCORE
+   */
+   public void setRAMPERCORE(String value);
+   public String getRAMPERCORE();
+
+   /**
+   * Protege name: INTERNODEBANDWIDTH
+   */
+   public void setINTERNODEBANDWIDTH(String value);
+   public String getINTERNODEBANDWIDTH();
+
+   /**
+   * Protege name: CPUSPEED
+   */
+   public void setCPUSPEED(String value);
+   public String getCPUSPEED();
+
+   /**
+   * Protege name: OPERATINGSYSTEM
+   */
+   public void setOPERATINGSYSTEM(String value);
+   public String getOPERATINGSYSTEM();
+
+   /**
+   * Protege name: ARCHITECTURE
+   */
+   public void setARCHITECTURE(String value);
+   public String getARCHITECTURE();
+
+   /**
+   * Protege name: OSVERSION
+   */
+   public void setOSVERSION(String value);
+   public String getOSVERSION();
+
+   /**
+   * Protege name: NODEDISKSPACE
+   */
+   public void setNODEDISKSPACE(String value);
+   public String getNODEDISKSPACE();
+
+}
