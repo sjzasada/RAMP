065eb11c923591cd6039170149ba7ec55b031cfa
Stefan Zasada <sjzasada@gmail.com>
2014-03-14 13:44:53 +0000

    updated user gui

--- src/uk/ac/ucl/chem/ccs/ramp/user/UserGui.java
+++ src/uk/ac/ucl/chem/ccs/ramp/user/UserGui.java
@@ -26,14 +26,19 @@ import java.awt.Insets;
 import java.io.File;
 import java.util.Vector;
 import javax.swing.JTextArea;
+import javax.swing.JTabbedPane;
+import javax.swing.JLabel;
 
 public class UserGui extends JFrame {
 
 	private final JPanel contentPanel = new JPanel();
+	
 	private JTable table;
 	private JTextArea textArea;
 	private UserAgent myAgent=null;
 	private Vector<Request> loadedRequests;
+	private final JPanel panel = new JPanel();
+	private final JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 	
 	/**
 	 * Launch the application.
@@ -63,114 +68,127 @@ public class UserGui extends JFrame {
 		
 		setBounds(100, 100, 799, 572);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		contentPanel.setLayout(null);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 11, 763, 236);
-		contentPanel.add(scrollPane);
+						getContentPane().add(contentPanel, BorderLayout.CENTER);
+						//panel.setLayout(null);
+						//tabbedPane.addTab("", null, null, null);
+						contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+						contentPanel.setLayout(null);
+						tabbedPane.setBounds(0, 0, 799, 549);
+						contentPanel.add(tabbedPane);
+						
+						JLabel lblNewLabel = new JLabel("New label");
+						panel.add(lblNewLabel);
+						
+						JPanel panel_1 = new JPanel();
+						tabbedPane.addTab("Buy Resources", null, panel_1, null);
+						panel_1.setLayout(null);
+						tabbedPane.addTab("Manage Purchases", null, panel, null);
+
+						
+						JScrollPane scrollPane = new JScrollPane();
+						scrollPane.setBounds(-1, 7, 773, 220);
+						panel_1.add(scrollPane);
+						
+						table = new JTable();
+						
+						
+								
+								
+								scrollPane.setViewportView(table);
+								
+								{
+									JPanel buttonPane = new JPanel();
+									buttonPane.setBounds(0, 239, 772, 41);
+									panel_1.add(buttonPane);
+									buttonPane.setLayout(null);
+									
+									JButton btnNewButton = new JButton("Load");
+									btnNewButton.setBounds(10, 0, 99, 35);
+									buttonPane.add(btnNewButton);
+									btnNewButton.addActionListener(new ActionListener() {
+										public void actionPerformed(ActionEvent e) {
+											JFileChooser fc = new JFileChooser();
+											fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+									
+											int returnVal = fc.showOpenDialog(UserGui.this);
+
+											
+											
+											if (returnVal == JFileChooser.APPROVE_OPTION) {
+																	
+												loadDir(fc.getSelectedFile().getAbsolutePath());
+	
+											}
+										}
+
+									});
+									
+									JButton btnPurchase = new JButton("Purchase");
+									btnPurchase.setBounds(109, 0, 106, 35);
+									btnPurchase.addActionListener(new ActionListener () {
+										public void actionPerformed (ActionEvent e) {
+											
+											
+											if (table.getSelectedRowCount() > 1) {
+												
+												int index[] = table.getSelectedRows();
+												
+												
+												//do this if more than one row is selected
+												Vector<Request> vec = new Vector<Request>();
+												
+												for (int i=0; i<index.length; i++) {
+													vec.add(loadedRequests.elementAt(index[i]));
+												}
+												
+												if (myAgent != null) {
+													myAgent.requestQuote(vec);
+												}
+												
+											} else if (table.getSelectedRowCount() == 1) {
+												
+												int index = table.getSelectedRow();
+												
+												Request req = loadedRequests.get(index);
+												
+												if (myAgent != null) {
+													myAgent.requestQuote(req);
+												}
+												
+											}
+											
+
+										}
+									});
+									
+									buttonPane.add(btnPurchase);
+												
+									JButton cancelButton = new JButton("Close");
+									cancelButton.setBounds(687, 0, 86, 35);
+									cancelButton.addActionListener(new ActionListener() {
+										public void actionPerformed(ActionEvent arg0) {
+											
+											if (myAgent != null) {
+												myAgent.takeDown();
+												UserGui.this.dispose();
+											} else {					
+												System.exit(0); //TODO: find out how to close properly
+											}
+										}
+									});
+									cancelButton.setActionCommand("Cancel");
+									buttonPane.add(cancelButton);
+								}
+								
+								JScrollPane scrollPane_1 = new JScrollPane();
+								scrollPane_1.setBounds(-1, 284, 773, 213);
+								panel_1.add(scrollPane_1);
+								
+								textArea = new JTextArea();
+								textArea.setLineWrap(true);
+								scrollPane_1.setViewportView(textArea);
 
 		this.setTitle("User Agent GUI");
-		
-		table = new JTable();
-
-
-		
-		
-		scrollPane.setViewportView(table);
-		
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setBounds(0, 258, 783, 41);
-			contentPanel.add(buttonPane);
-			buttonPane.setLayout(null);
-			
-			JButton btnNewButton = new JButton("Load");
-			btnNewButton.setBounds(10, 0, 87, 30);
-			buttonPane.add(btnNewButton);
-			btnNewButton.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					JFileChooser fc = new JFileChooser();
-					fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-			
-					int returnVal = fc.showOpenDialog(UserGui.this);
-
-					
-					
-					if (returnVal == JFileChooser.APPROVE_OPTION) {
-											
-						loadDir(fc.getSelectedFile().getAbsolutePath());
-	
-					}
-				}
-
-			});
-			
-			JButton btnPurchase = new JButton("Purchase");
-			btnPurchase.setBounds(107, 0, 87, 30);
-			btnPurchase.addActionListener(new ActionListener () {
-				public void actionPerformed (ActionEvent e) {
-					
-					
-					if (table.getSelectedRowCount() > 1) {
-						
-						int index[] = table.getSelectedRows();
-						
-						
-						//do this if more than one row is selected
-						Vector<Request> vec = new Vector<Request>();
-						
-						for (int i=0; i<index.length; i++) {
-							vec.add(loadedRequests.elementAt(index[i]));
-						}
-						
-						if (myAgent != null) {
-							myAgent.requestQuote(vec);
-						}
-						
-					} else if (table.getSelectedRowCount() == 1) {
-						
-						int index = table.getSelectedRow();
-						
-						Request req = loadedRequests.get(index);
-						
-						if (myAgent != null) {
-							myAgent.requestQuote(req);
-						}
-						
-					}
-					
-
-				}
-			});
-			
-			buttonPane.add(btnPurchase);
-						
-			JButton cancelButton = new JButton("Close");
-			cancelButton.setBounds(703, 0, 70, 30);
-			cancelButton.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent arg0) {
-					
-					if (myAgent != null) {
-						myAgent.takeDown();
-						UserGui.this.dispose();
-					} else {					
-						System.exit(0); //TODO: find out how to close properly
-					}
-				}
-			});
-			cancelButton.setActionCommand("Cancel");
-			buttonPane.add(cancelButton);
-		}
-		
-		JScrollPane scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(10, 310, 763, 213);
-		contentPanel.add(scrollPane_1);
-		
-		textArea = new JTextArea();
-		textArea.setLineWrap(true);
-		scrollPane_1.setViewportView(textArea);
 		
 		updateDisplay ();
 		
